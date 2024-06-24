9f6253c55f542c91d99d8e476d58cbf4e7d3c253
ByronLuke <byronpc18@icloud.com>
2024-06-24 16:32:32 -0500

    Error handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { type ReactNode, useEffect, useState } from "react";
 import { get } from "./util/http";
 import BlogPosts, { BlogPost } from "./components/BlogPosts.tsx";
-import fetchingImage from './assets/data-fetching.png'
+import fetchingImage from "./assets/data-fetching.png";
+import ErrorMessage from "./components/ErrorMessage.tsx";
 
 type RawDataBlogPost = {
   id: number;
@@ -12,36 +13,56 @@ type RawDataBlogPost = {
 
 function App() {
   const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState<string>();
 
   useEffect(() => {
     async function fetchPosts() {
-      const data = (await get(
-        "https://jsonplaceholder.typicode.com/posts"
-      )) as RawDataBlogPost[];
+      setIsFetching(true);
+      try {
+        const data = (await get(
+          "https://jsonplaceholder.typicode.com/posts"
+        )) as RawDataBlogPost[];
 
-      const blogPosts : BlogPost[]= data.map((rawPost) => {
-        return {
-          id: rawPost.id,
-          title: rawPost.title,
-          text: rawPost.body,
-        };
-      });
+        const blogPosts: BlogPost[] = data.map((rawPost) => {
+          return {
+            id: rawPost.id,
+            title: rawPost.title,
+            text: rawPost.body,
+          };
+        });
+        setFetchedPosts(blogPosts);
+      } catch (error) {
+        if (error instanceof Error) {
+          setError(error.message);
+        }
+      }
 
-      setFetchedPosts(blogPosts);
+      setIsFetching(false);
     }
     fetchPosts();
   }, []);
 
-  let content : ReactNode;
+  let content: ReactNode;
 
-  if(fetchedPosts){
-    content = <BlogPosts posts={fetchedPosts}/>
+  if(error){
+    content=<ErrorMessage text={error}/>
   }
 
-  return <main>
-    <img src={fetchingImage} alt="Abstract Data fetching image process"/>
-  {content}
-  </main>;
+  if (fetchedPosts) {
+    content = <BlogPosts posts={fetchedPosts} />;
+  }
+
+  if (isFetching) {
+    content = <p id="loading-fallback">Fetching Posts...</p>;
+  }
+
+  return (
+    <main>
+      <img src={fetchingImage} alt="Abstract Data fetching image process" />
+      {content}
+    </main>
+  );
 }
 
 export default App;
